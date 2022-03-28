5feb5c63b6b6d48e2d8774514c6ff6f9cf9a1e1e
OvchinnikovKirill <Ovchinnikovk758@gmail.com>
2022-03-28 18:31:11 +0300

    meeting with components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,47 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello World!</h2>
+}
+
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   }
+//   return <input 
+//     type="text" 
+//     placeholder={holder} 
+//     style={styledField}/>
+// }
+
+class Field extends Component {
+  render() {
+    const holder = 'Enter here';
+    const styledField = {
+    width: '300px'
+  }
+
+  return <input 
+          type="text" 
+          placeholder={holder} 
+          style={styledField}/>
+  }
+}
+
+const Btn = function() {
+  const text = 'click';
+  const logged = false;
+  return <button>{logged ? text : 'noclick'}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-const text = 'Hello world!';
-
-const elem = (
-  <div>
-    <h2 className='text'>{text}</h2>
-    <input type="text"/>
-    <button tabIndex={0}>button</button>
-  </div>
-);
 
 ReactDOM.render(
-  elem,
+  <App/>,
   document.getElementById('root')
 );
